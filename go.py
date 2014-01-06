60cb1be5cadca4aebf4c1a199dffe7d9b4f09e97
Konrad Iturbe <github@chernowii.com>
2014-01-06 23:11:12 +0100

    Create go.py

---
+++ go.py
@@ -0,0 +1,9 @@
+# Go.Py by Konrad Iturbe (github: @konradit) www.chernowii.com
+# This go.py will wait X seconds, turn camera on and record for Y seconds.
+import urllib2
+import time
+
+time.sleep (X)
+urllib2(xxxx)
+time.sleep (4)
+urllib2 (zzzzzzz)
