6e743ef9dead3aef1fe201c28dea4a3031e7b97c
Konrad Iturbe <github@chernowii.com>
2014-01-07 10:26:26 +0100

    Update go.py

--- go.py
+++ go.py
@@ -5,8 +5,13 @@ import urllib2
 import time
 
 time.sleep (X)
-urllib2(xxxx)
+print "Turn GoPro ON"
+urllib2.urlopen(http://10.5.5.9/bacpac/PW?t=WIFIPASSWORD&p=%01)
 time.sleep (4)
-urllib2 (zzzzzzz)
+print "Start recording"
+urllib2.urlopen(http://10.5.5.9/bacpac/SH?t=WIFIPASSWORD&p=%01)
 time.sleep (Y)
-urllib2 (aaaaaaa)
+print "Stop recording"
+urllib2.urlopen(http://10.5.5.9/bacpac/SH?t=WIFIPASSWORD&p=%00)
+time.sleep (3)
+print "Be a HERO"
