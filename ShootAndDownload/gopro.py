eb343e015d6bc26e9911db0dce951250329211b2
Konrad Iturbe <github@chernowii.com>
2014-01-19 19:50:33 +0100

    commit

---
+++ ShootAndDownload/gopro.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from time import sleep
+import urllib2
+
+while True:
+time.sleep(X)
+urllib2.urlopen ("hhh")
+time.sleep(3)
+urllib.urlretrieve ("http://10.5.5.9:8080/videos/DCIM/100GOPRO/GOPR0001.JPG", "jpg.jpg")
+time.sleep(6)
+urllib2.urlopen ("hhhhhhh")
